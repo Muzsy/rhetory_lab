05f8cf600d456181ee095721e2ecc41bc0dfd5e1
Muszy <muszy@rhetoric.dev>
2026-04-19 23:19:20 +0200

    Try EmptyLocalStorage to debug PKCE issue

--- rhetorium/lib/main.dart
+++ rhetorium/lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'env.dart';
 import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  await SharedPreferences.getInstance();
-  
   await Supabase.initialize(
     url: Env.supabaseUrl,
     anonKey: Env.supabaseAnonKey,
     authOptions: FlutterAuthClientOptions(
-      localStorage: SharedPreferencesLocalStorage(
-        persistSessionKey: 'sb-dcvioxuxjkbsxccnoszl-auth-token',
-      ),
+      localStorage: const EmptyLocalStorage(),
     ),
   );
   
